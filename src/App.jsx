3bee235d05325de3938466e7d6103da10b274805
Насырбек <64436023+Atamazow@users.noreply.github.com>
2024-08-22 01:41:57 +0300

    Добавил еще один блок, сделал изменение в стилях

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Rombik from "./components/Rombik/Rombik";
 import Carousel from "./components/Carousel/Carousel";
+import Page4 from "./components/Page4/Page4";
 
 
 
@@ -8,6 +9,7 @@ function App(props) {
     return (
         <div className="App-wrapper" >
              <Rombik/>
+            <Page4/>
              <Carousel/>
          </div>
     );
