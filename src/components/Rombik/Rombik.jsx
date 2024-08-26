1465e260c12768e61d629af2783f3ef5d261a299
Насырбек <64436023+Atamazow@users.noreply.github.com>
2024-08-26 05:38:02 +0300

    Добавил появление картинок при наведении, добавил плавное появление

--- src/components/Rombik/Rombik.jsx
+++ src/components/Rombik/Rombik.jsx
@@ -1,40 +1,76 @@
-import React from 'react';
-import subCart from '../../assets/carouselImage/Subtract.svg';
-import headerRombDuga from '../../assets/carouselImage/headerDugaRomb.svg'
-import './Rombik.css';
+import React from "react";
+import subCart from "../../assets/carouselImage/Subtract.svg";
+import headerRombDuga from "../../assets/carouselImage/headerDugaRomb.svg";
+import imageRomb from "../../assets/image/Subtract.png";
+import "./HoverImageRomb.css";
+import "./Rombik.css";
 
 const cardRombik = [
-    {id:1,title: 'Real Estate Mismanagement', text: 'Aixland\'s Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n'},
-    {id:2,title: 'Real Estate Mismanagement', text: 'Aixland\'s Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n'},
-    {id:3,title: 'Real Estate Mismanagement', completed: true, text: 'Aixland\'s Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n'},
-    {id:4,title: 'Real Estate Mismanagement', text: 'Aixland\'s Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n'},
-    {id:5,title: 'Real Estate Mismanagement', text: 'Aixland\'s Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n'},
+  {
+    id: 1,
+    image: imageRomb,
+    title: "Real Estate Mismanagement",
+    text: "Aixland's Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n",
+  },
+  {
+    id: 2,
+    image: imageRomb,
+    title: "Real Estate Mismanagement",
+    text: "Aixland's Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n",
+  },
+  {
+    id: 3,
+    image: imageRomb,
+    title: "Real Estate Mismanagement",
+    completed: true,
+    text: "Aixland's Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n",
+  },
+  {
+    id: 4,
+    image: imageRomb,
+    title: "Real Estate Mismanagement",
+    text: "Aixland's Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n",
+  },
+  {
+    id: 5,
+    image: imageRomb,
+    title: "Real Estate Mismanagement",
+    text: "Aixland's Solution: Aixland integrates physical assets with the digital twins, enabling real-time management and seamless coordination, thereby improving operational efficiency.\n",
+  },
+];
+
+const imageRombik = [
+  { id: 1, image: imageRomb },
+  { id: 2, image: imageRomb },
+  { id: 3, image: imageRomb },
+  { id: 4, image: imageRomb },
+  { id: 5, image: imageRomb },
 ];
 
 const Rombik = () => {
-    return (
-
-            <div className="wrapper--romb">
-                <img className='headerDugaRomb' src={headerRombDuga} alt=""/>
-                <div className="hexStepCard--wrapper">
-                    {cardRombik.map((card, index) => (
-                        <div className='card-block' key={index}>
-                            <div>
-                                <img className='subCartImage' src={subCart} alt=""/>
-                            </div>
-                            <div className='cardRomb--textTitle'>
-                                <div className="title--cardRombik">
-                                    {card.title}
-                                </div>
-                                <div className="text--cardRombik">
-                                    {card.text}
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <div className="wrapper--romb">
+      <div className="hexStepCard--wrapper">
+        {cardRombik.map((card, index) => (
+          <div style={{ transition: "1s" }} className="card-block" key={index}>
+            <img
+              className={`image--romb image--romb-${index + 1}`}
+              style={{ transition: "1s" }}
+              src={card.image}
+              alt=""
+            />
+            <div>
+              <img className="subCartImage" src={subCart} alt="" />
             </div>
-     );
+            <div className="cardRomb--textTitle">
+              <div className="title--cardRombik">{card.title}</div>
+              <div className="text--cardRombik">{card.text}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Rombik;
